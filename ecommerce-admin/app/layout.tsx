398d97f46b77519c45bb0c05d9a984848939090a
Scylla23 <2019bcs044@sggs.ac.in>
2023-09-21 22:59:06 +0530

    overview

--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -1,34 +1,39 @@
-import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from '@/providers/modal-provider'
+import { ToastProvider } from '@/providers/toast-provider'
+import { ThemeProvider } from '@/providers/theme-provider'
 
+import './globals.css'
 
-import { ModalProvider } from "@/providers/modal-provider";
-import { ToastProvider } from "@/providers/toast-provider";
+const inter = Inter({ subsets: ['latin'] })
 
-const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  title: 'Dashboard',
+  description: 'E-Commerce Dashboard',
+}
 
-export const metadata: Metadata = {
-  title: "Admin DashBoard",
-  description: "Admin DashBoard",
-};
-
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToastProvider/>
-            <ModalProvider/>
+          <ThemeProvider 
+            attribute="class" 
+            defaultTheme="system" 
+            enableSystem
+          >
+            <ToastProvider />
+            <ModalProvider />
             {children}
-          </body>
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
